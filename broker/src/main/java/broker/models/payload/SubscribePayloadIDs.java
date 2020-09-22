3982fe1477adc8af0e74caf27d0bbb53c5acefe0
Dragunova <ana448262@gmail.com>
2020-09-22 21:34:58 +0300

    Создание файлов Executor'ов и Payload'ов для Подписки

--- broker/src/main/java/broker/models/payload/SubscribePayloadIDs.java
+++ broker/src/main/java/broker/models/payload/SubscribePayloadIDs.java
@@ -1,10 +1,13 @@
 package broker.models.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SubscribePayloadIDs implements Payload{
+
+    @JsonProperty("Subscribers")
     private int[] ArraySubscribersID;
 }
--- broker/src/main/java/broker/models/payload/SubscribePayloadNonExisting.java
+++ broker/src/main/java/broker/models/payload/SubscribePayloadNonExisting.java
@@ -1,9 +1,12 @@
 package broker.models.payload;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SubscribePayloadNonExisting implements Payload{
     private final Code code;
+
+    @JsonProperty("Non-Existing-Subscribers")
     private int[] ArrayNonExistingSubscribersID;
 }
